bc80dc6ef7315c5ecbe764a00b795f0eb73aa0bb
Andrew Stewart <andrew@stwrt.ca>
2016-12-30 00:39:23 -0800

    Add fuzzy::eq(a, b) function
    This can be used to compare characters case-insensitively.

--- src/fuzzy/mod.rs
+++ src/fuzzy/mod.rs
@@ -2,3 +2,26 @@ mod score;
 pub mod consts;
 
 pub use self::score::*;
+
+/// Compares two characters case-insensitively
+///
+/// # Examples
+///
+/// ```
+/// assert!(rff::fuzzy::eq('a', 'A'));
+/// ```
+pub fn eq(a: char, b: char) -> bool {
+    a.to_uppercase().eq(b.to_uppercase())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_eq() {
+        assert!(eq('a', 'A'));
+        assert!(eq('山', '山'));
+        assert!(!eq('a', 'b'));
+    }
+}
