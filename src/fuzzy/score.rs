5874d6bdb74c52ef375ebebf06d995e2e2cff7a5
Andrew Stewart <andrew@stwrt.ca>
2016-12-30 17:48:19 -0800

    Implement PartialOrd, PartialEq for Score
    This allows Scores to be compared and sorted.

--- src/fuzzy/score.rs
+++ src/fuzzy/score.rs
@@ -1,3 +1,5 @@
+use std::cmp::Ordering;
+
 #[derive(Copy, Clone, Debug)]
 pub struct Score {
     /// The computed score value
@@ -20,7 +22,38 @@ impl Score {
     }
 }
 
+impl PartialOrd for Score {
+    fn partial_cmp(&self, other: &Score) -> Option<Ordering> {
+        self.value.partial_cmp(&other.value)
+    }
+}
+
+impl PartialEq for Score {
+    fn eq(&self, other: &Score) -> bool {
+        self.value == other.value
+    }
+}
+
+
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn test_eq() {
+        let a = Score::new(1.0);
+        let b = Score::new(1.0);
+        assert_eq!(a, b);
+    }
+
+    #[test]
+    fn test_cmp() {
+        let a = Score::new(2.0);
+        let b = Score::new(1.0);
+        assert!(a > b);
+        assert!(b < a);
+
+        let b = Score::new(2.0);
+        assert!(a == b);
+    }
 }
