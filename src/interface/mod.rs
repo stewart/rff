c0acc44074089194ddcfdca118420967dc03b455
Andrew Stewart <andrew@stwrt.ca>
2016-12-31 19:59:38 -0800

    Add terminal to Interface, use for printing, events

--- src/interface/mod.rs
+++ src/interface/mod.rs
@@ -1,19 +1,40 @@
 mod terminal;
 
+use std::io::Write;
+use self::terminal::{Terminal, Event, Key};
+
 pub struct Interface {
-    choices: Vec<String>
+    choices: Vec<String>,
+    terminal: Terminal
 }
 
 impl Interface {
     /// Creates a new Interface with the provided input choices.
     pub fn with_choices(choices: Vec<String>) -> Interface {
+        let mut term = Terminal::from("/dev/tty").unwrap();
+        term.set_raw_mode().unwrap();
+
         Interface {
-            choices: choices
+            choices: choices,
+            terminal: term
         }
     }
 
     // Starts the interface
     pub fn run(&mut self) {
-        println!("{} choices to search through", self.choices.len());
+        let ref mut term = self.terminal;
+
+        write!(term, "{} choices to search through\r\n", self.choices.len());
+
+        for event in term.events().unwrap() {
+            match event {
+                Ok(Event::Key(Key::Ctrl('c'))) => {
+                    return;
+                },
+
+                Ok(event) => write!(term, "{:?}\r\n", event),
+                Err(err) => write!(term, "{:?}\r\n", err),
+            };
+        }
     }
 }
