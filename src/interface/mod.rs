f526d04cbcc96a0b536355c8093763a78faec6c2
Andrew Stewart <andrew@stwrt.ca>
2017-01-01 00:21:19 -0800

    Add search query, split out render method

--- src/interface/mod.rs
+++ src/interface/mod.rs
@@ -1,11 +1,13 @@
 mod terminal;
 mod ansi;
 
-use std::io::Write;
+use std::io::{self, Write};
 use self::terminal::{Terminal, Event, Key};
+use self::ansi::{clear, cursor};
 
 pub struct Interface {
     choices: Vec<String>,
+    search: String,
     terminal: Terminal
 }
 
@@ -17,25 +19,41 @@ impl Interface {
 
         Interface {
             choices: choices,
+            search: String::new(),
             terminal: term
         }
     }
 
     // Starts the interface
     pub fn run(&mut self) {
-        let ref mut term = self.terminal;
+        self.render().expect("Unable to render");
 
-        write!(term, "{} choices to search through\r\n", self.choices.len());
-
-        for event in term.events().unwrap() {
+        for event in self.terminal.events().unwrap() {
             match event {
                 Ok(Event::Key(Key::Ctrl('c'))) => {
                     return;
                 },
 
-                Ok(event) => write!(term, "{:?}\r\n", event),
-                Err(err) => write!(term, "{:?}\r\n", err),
+                Ok(Event::Key(Key::Char(ch))) => {
+                    self.search.push(ch);
+                    self.render().expect("Unable to render");
+                },
+
+                Ok(Event::Key(Key::Backspace)) => {
+                    self.search.pop();
+                    self.render().expect("Unable to render");
+                },
+
+                Ok(_) => {}
+                Err(err) => write!(self.terminal, "{:?}", err).unwrap(),
             };
         }
+    }
+
+    fn render(&mut self) -> io::Result<()> {
+        let ref mut term = self.terminal;
+        write!(term, "{}{}", clear::Line, cursor::Column(1))?;
+        write!(term, "> {}", self.search)?;
+        Ok(())
     }
 }
