2fa778c2583b862d87e6cf993a3b53055b1236c9
Andrew Stewart <andrew@stwrt.ca>
2016-12-31 18:52:20 -0800

    Implement Read/Write for Terminal
    This accesses the underlying File.

--- src/interface/terminal/mod.rs
+++ src/interface/terminal/mod.rs
@@ -1,11 +1,12 @@
 use std::fs::{File, OpenOptions};
-use std::io::Result;
+use std::io::{self, Write, Read, Result};
 
 pub struct Terminal {
     file: File
 }
 
 impl Terminal {
+    /// Creates a new Terminal from the provided filename
     pub fn from(filename: &str) -> Result<Terminal> {
         let file = OpenOptions::new().write(true).read(true).open(filename)?;
 
@@ -14,5 +15,21 @@ impl Terminal {
         };
 
         Ok(terminal)
+    }
+}
+
+impl Read for Terminal {
+    fn read(&mut self, buf: &mut [u8]) -> Result<usize> {
+        self.file.read(buf)
+    }
+}
+
+impl Write for Terminal {
+    fn write(&mut self, buf: &[u8]) -> Result<usize> {
+        self.file.write(buf)
+    }
+
+    fn flush(&mut self) -> Result<()> {
+        self.file.flush()
     }
 }
