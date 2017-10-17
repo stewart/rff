3b0cb22d42a45e3c61c53c01be9608d7c36293a1
Andrew Stewart <andrew@stwrt.ca>
2017-10-17 11:31:50 -0700

    Filter lines by search term

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,16 @@
 extern crate rff;
 
 use std::env;
+use rff::{stdin, matcher};
 
 fn main() {
     if let Some(search_term) = env::args().nth(1) {
-        let lines = rff::stdin::slurp();
+        let lines = stdin::slurp();
+
+        let lines: Vec<_> = lines.iter()
+            .filter(|line| { matcher::matches(&search_term, line) })
+            .collect();
+
         println!("Lines:\n{:?}", lines);
         println!("Search: {}", search_term);
     }
