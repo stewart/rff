e5717abacb8f60a0273d598617f763cd43c5c8ca
Andrew Stewart <andrew@stwrt.ca>
2017-10-22 12:03:03 -0700

    Add benchmark mode

--- src/main.rs
+++ src/main.rs
@@ -16,27 +16,50 @@ fn main() {
                 required(true).
                 index(1)
         ).
+        arg(
+            Arg::with_name("benchmark").
+                long("benchmark").
+                help("Run rff in benchmark mode")
+        ).
         get_matches();
 
     let query = matches.value_of("QUERY").unwrap();
     let lines = stdin::slurp();
 
-    let mut lines: Vec<_> = lines.iter()
-        .filter_map(|line| {
-            if matcher::matches(&query, line) {
-                Some((line, scorer::score(&query, line)))
-            } else {
-                None
-            }
-        })
-        .collect();
+    if matches.is_present("benchmark") {
+        // in benchmark mode, we run the match/score/sort loop 100 times
+        for _ in 0..100 {
+            lines.
+                iter().
+                filter_map(|line| {
+                    if matcher::matches(&query, line) {
+                        Some((line, scorer::score(&query, line)))
+                    } else {
+                        None
+                    }
+                }).
+                collect::<Vec<_>>().
+                sort_by(|a, b| a.1.partial_cmp(&b.1).unwrap().reverse());
+        }
+    } else {
+        let mut lines: Vec<_> = lines.iter()
+            .filter_map(|line| {
+                if matcher::matches(&query, line) {
+                    Some((line, scorer::score(&query, line)))
+                } else {
+                    None
+                }
+            })
+            .collect();
 
-    lines.sort_by(|a, b| a.1.partial_cmp(&b.1).unwrap().reverse());
+        lines.sort_by(|a, b| a.1.partial_cmp(&b.1).unwrap().reverse());
 
-    let stdout = io::stdout();
-    let mut stdout = BufWriter::new(stdout.lock());
+        let stdout = io::stdout();
+        let mut stdout = BufWriter::new(stdout.lock());
 
-    for line in &lines {
-        writeln!(stdout, "{}", line.0).unwrap();
+        for line in &lines {
+            writeln!(stdout, "{}", line.0).unwrap();
+        }
     }
+
 }
