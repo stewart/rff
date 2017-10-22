804fbdf5bd66819042185603c5aa76c896eacecd
Andrew Stewart <andrew@stwrt.ca>
2017-10-21 20:11:17 -0700

    End-to-end working

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,25 @@
 extern crate rff;
 
 use std::env;
-use rff::{stdin, matcher};
+use std::io::{self, Write, BufWriter};
+use rff::{stdin, matcher, scorer};
 
 fn main() {
     if let Some(search_term) = env::args().nth(1) {
         let lines = stdin::slurp();
 
-        let lines: Vec<_> = lines.iter()
+        let mut lines: Vec<_> = lines.iter()
             .filter(|line| { matcher::matches(&search_term, line) })
+            .map(|line| { (line, scorer::score(&search_term, line)) })
             .collect();
 
-        println!("Lines:\n{:?}", lines);
-        println!("Search: {}", search_term);
+        lines.sort_by(|a, b| a.1.partial_cmp(&b.1).unwrap().reverse());
+
+        let stdout = io::stdout();
+        let mut stdout = BufWriter::new(stdout.lock());
+
+        for line in &lines {
+            writeln!(stdout, "{}", line.0).unwrap();
+        }
     }
 }
