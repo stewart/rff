934c118e42406864a00fcfddb4d78548ab2f3c63
Andrew Stewart <andrew@stwrt.ca>
2017-11-04 13:47:53 -0700

    Extract work out of main()

--- src/main.rs
+++ src/main.rs
@@ -29,28 +29,9 @@ fn main() {
     let lines = stdin::slurp();
 
     if matches.is_present("benchmark") {
-        // in benchmark mode, we run the match/score/sort loop 100 times
-        for _ in 0..100 {
-            lines
-                .iter()
-                .filter_map(|line| match_and_score(query, line))
-                .collect::<Vec<_>>()
-                .sort_by(|a, b| a.1.partial_cmp(&b.1).unwrap().reverse());
-        }
+        benchmark(query, lines);
     } else {
-        let mut lines: Vec<_> = lines
-            .iter()
-            .filter_map(|line| match_and_score(query, line))
-            .collect();
-
-        lines.sort_by(|a, b| a.1.partial_cmp(&b.1).unwrap().reverse());
-
-        let stdout = io::stdout();
-        let mut stdout = BufWriter::new(stdout.lock());
-
-        for line in &lines {
-            writeln!(stdout, "{}", line.0).unwrap();
-        }
+        search(query, lines);
     }
 }
 
@@ -59,5 +40,32 @@ fn match_and_score<'a>(needle: &str, haystack: &'a str) -> Option<Match<'a>> {
         Some((haystack, scorer::score(needle, haystack)))
     } else {
         None
+    }
+}
+
+fn benchmark(needle: &str, lines: Vec<String>) {
+    // in benchmark mode, we run the match/score/sort loop 100 times
+    for _ in 0..100 {
+        lines
+            .iter()
+            .filter_map(|line| match_and_score(needle, line))
+            .collect::<Vec<_>>()
+            .sort_by(|a, b| a.1.partial_cmp(&b.1).unwrap().reverse());
+    }
+}
+
+fn search(needle: &str, lines: Vec<String>) {
+    let mut lines: Vec<_> = lines
+        .iter()
+        .filter_map(|line| match_and_score(needle, line))
+        .collect();
+
+    lines.sort_by(|a, b| a.1.partial_cmp(&b.1).unwrap().reverse());
+
+    let stdout = io::stdout();
+    let mut stdout = BufWriter::new(stdout.lock());
+
+    for line in &lines {
+        writeln!(stdout, "{}", line.0).unwrap();
     }
 }
