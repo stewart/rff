02a471d2b038e53ed6c368e1dc2d3fa6b16cae42
Andrew Stewart <andrew@stwrt.ca>
2017-11-07 18:22:26 -0800

    Add match_and_score_with_positions

--- src/lib.rs
+++ src/lib.rs
@@ -10,10 +10,20 @@ pub mod matrix;
 pub mod scorer;
 
 pub type Match<'a> = (&'a str, f64);
+pub type MatchWithPositions<'a> = (&'a str, f64, Vec<usize>);
 
 pub fn match_and_score<'a>(needle: &str, haystack: &'a str) -> Option<Match<'a>> {
     if matcher::matches(needle, haystack) {
         Some((haystack, scorer::score(needle, haystack)))
+    } else {
+        None
+    }
+}
+
+pub fn match_and_score_with_positions<'a>(needle: &str, haystack: &'a str) -> Option<MatchWithPositions<'a>> {
+    if matcher::matches(needle, haystack) {
+        let (score, positions) = scorer::score_with_positions(needle, haystack);
+        Some((haystack, score, positions))
     } else {
         None
     }
