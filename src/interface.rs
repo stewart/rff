f4f4622276745a50ddf9c4f6e827727e507d6619
Andrew Stewart <andrew@stwrt.ca>
2017-11-11 13:49:38 -0800

    Add search term to interface

--- src/interface.rs
+++ src/interface.rs
@@ -23,6 +23,7 @@ impl From<terminal::Error> for Error {
 
 pub struct Interface {
     lines: Vec<String>,
+    search: String,
     terminal: Terminal,
 }
 
@@ -30,18 +31,12 @@ impl Interface {
     pub fn new(lines: Vec<String>) -> Interface {
         Interface {
             lines: lines,
+            search: String::new(),
             terminal: Terminal::from("/dev/tty").unwrap(),
         }
     }
 
-    pub fn run(&mut self) -> Result<String, Error> {
-        let mut term = BufWriter::new(&mut self.terminal);
-        let mut search = String::new();
-
-        for line in &self.lines {
-            write!(term, "{}\n", line)?;
-        }
-
-        return Ok(search)
+    pub fn run(&mut self) -> Result<&str, Error> {
+        Ok(&self.search)
     }
 }
