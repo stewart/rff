9b22564d80c053d087709b34419ea3c8bfd650ea
Andrew Stewart <andrew@stwrt.ca>
2017-11-11 13:41:29 -0800

    Add custom, centralized Error for Interface

--- src/interface.rs
+++ src/interface.rs
@@ -1,6 +1,25 @@
 use std::io::{self, Write, BufWriter};
 
-use terminal::Terminal;
+use terminal::{self, Terminal};
+
+#[derive(Debug)]
+pub enum Error {
+    Exit,
+    Write(io::Error),
+    Reset(terminal::Error)
+}
+
+impl From<io::Error> for Error {
+    fn from(err: io::Error) -> Error {
+        Error::Write(err)
+    }
+}
+
+impl From<terminal::Error> for Error {
+    fn from(err: terminal::Error) -> Error {
+        Error::Reset(err)
+    }
+}
 
 pub struct Interface {
     lines: Vec<String>,
@@ -15,7 +34,7 @@ impl Interface {
         }
     }
 
-    pub fn run(&mut self) -> Result<String, io::Error> {
+    pub fn run(&mut self) -> Result<String, Error> {
         let mut term = BufWriter::new(&mut self.terminal);
         let mut search = String::new();
 
