238cd9f7f532df9265db591db6d229420fa971b2
Andrew Stewart <andrew@stwrt.ca>
2018-09-29 01:20:33 -0700

    Add basic matching via CLI
    It's a start!

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,31 @@
 #![deny(unused_must_use)]
 
 extern crate clap;
+extern crate rff;
 
 mod stdin;
 
-use clap::App;
+use clap::{App,Arg};
+use rff::*;
 
 fn main() {
     let args = App::new("rff")
         .version(env!("CARGO_PKG_VERSION"))
         .author("Andrew Stewart <andrew@stwrt.ca>")
+        .arg(Arg::with_name("NEEDLE").
+             help("Term to search for").
+             required(true).
+             index(1))
         .get_matches();
 
     let choices = stdin::slurp();
 
-    println!("args: {:?}", args);
-    println!("num of choices: {}", choices.len());
+    let needle = args.value_of("NEEDLE").expect("Unable to get needle");
+
+    let query: Vec<String> = choices.
+        into_iter().
+        filter(|haystack| matches(&needle, &haystack)).
+        collect();
+
+    print!("{}", query.join("\n"));
 }
