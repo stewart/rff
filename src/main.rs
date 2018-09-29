97932cdb8edbdaa19140b93b45bba43d3b6678a8
Andrew Stewart <andrew@stwrt.ca>
2018-09-29 00:22:07 -0700

    Remove unnecessary stuff

--- src/main.rs
+++ src/main.rs
@@ -3,22 +3,8 @@ extern crate clap;
 mod stdin;
 
 use clap::App;
-use std::process;
-
-// A Result shorthand for the CLI integration
-type Result<T> = std::result::Result<T, &'static str>;
 
 fn main() {
-    match run() {
-        Ok(_) => process::exit(0),
-        Err(err) => {
-            eprintln!("{}", err);
-            process::exit(1);
-        }
-    }
-}
-
-fn run() -> Result<()> {
     let args = App::new("rff")
         .version(env!("CARGO_PKG_VERSION"))
         .author("Andrew Stewart <andrew@stwrt.ca>")
@@ -28,6 +14,4 @@ fn run() -> Result<()> {
 
     println!("args: {:?}", args);
     println!("num of choices: {}", choices.len());
-
-    Ok(())
 }
--- src/stdin.rs
+++ src/stdin.rs
@@ -1,8 +1,7 @@
 use std::io::{self, BufRead};
 
-pub type InputLines = Vec<String>;
-
-pub fn slurp() -> InputLines {
+/// Reads all available lines of input from stdin.
+pub fn slurp() -> Vec<String> {
     let stdin = io::stdin();
     let stdin = stdin.lock();
     read_lines(stdin)
@@ -10,6 +9,6 @@ pub fn slurp() -> InputLines {
 
 // This fn is extracted from the above to work around a lifetime issue.
 // TODO: Investigate if this is necessary after NLL hits stable.
-fn read_lines<T: BufRead>(buf: T) -> InputLines {
+fn read_lines<T: BufRead>(buf: T) -> Vec<String> {
     buf.lines().map(Result::unwrap).collect()
 }
