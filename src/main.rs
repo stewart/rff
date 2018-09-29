92c07621f2a77fa8d3a4a318197e2359184f22ad
Andrew Stewart <andrew@stwrt.ca>
2018-09-29 14:57:33 -0700

    Run rust-fmt
    A few small discrepancies in `main.rs`.

--- src/main.rs
+++ src/main.rs
@@ -5,28 +5,30 @@ extern crate rff;
 
 mod stdin;
 
-use clap::{App,Arg};
+use clap::{App, Arg};
 use rff::*;
 
 fn main() {
     let args = App::new("rff")
         .version(env!("CARGO_PKG_VERSION"))
         .author("Andrew Stewart <andrew@stwrt.ca>")
-        .arg(Arg::with_name("NEEDLE").
-             help("Term to search for").
-             required(true).
-             index(1))
+        .arg(
+            Arg::with_name("NEEDLE")
+                .help("Term to search for")
+                .required(true)
+                .index(1),
+        )
         .get_matches();
 
     let choices = stdin::slurp();
 
     let needle = args.value_of("NEEDLE").expect("Unable to get needle");
 
-    let results: Vec<(f64, String)> = choices.
-        into_iter().
-        filter(|haystack| matches(&needle, &haystack)).
-        map(|haystack| (score(&needle, &haystack), haystack)).
-        collect();
+    let results: Vec<(f64, String)> = choices
+        .into_iter()
+        .filter(|haystack| matches(&needle, &haystack))
+        .map(|haystack| (score(&needle, &haystack), haystack))
+        .collect();
 
     for (score, candidate) in results {
         println!("{}: {}", score, candidate);
