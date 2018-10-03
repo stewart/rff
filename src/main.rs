6e7bfc253006aad4b38d2a6250cb8a206d59399a
Andrew Stewart <andrew@stwrt.ca>
2018-10-03 13:25:43 -0700

    Simplify reading choices from STDIN, inline module
    Handy trick: Result implements the FromIterator trait, so we can use
    .collect() to automagically turn an Iterator of Vec<Result<String>> into
    Result<Vec<String>>.

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@
 extern crate clap;
 extern crate rff;
 
-mod stdin;
+use std::io::{self, BufRead, Result};
 
 use clap::{App, Arg};
 use rff::*;
@@ -19,9 +19,9 @@ fn main() {
                 .index(1),
         ).get_matches();
 
-    let choices = stdin::slurp();
+    let choices = read_choices().expect("unable to read choices from stdin");
 
-    let needle = args.value_of("NEEDLE").expect("Unable to get needle");
+    let needle = args.value_of("NEEDLE").expect("unable to get needle");
 
     let results: Vec<(f64, String)> = choices
         .into_iter()
@@ -32,4 +32,11 @@ fn main() {
     for (score, candidate) in results {
         println!("{}: {}", score, candidate);
     }
+}
+
+/// Reads all available lines of input from stdin.
+fn read_choices() -> Result<Vec<String>> {
+    let stdin = io::stdin();
+    let lines = stdin.lock().lines().collect();
+    lines
 }
